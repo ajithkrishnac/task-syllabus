990db355394de675753c152cf840cca31c1847f6
Ajith Krishna C <53771121+ajithkrishnac@users.noreply.github.com>
2024-02-20 03:46:07 +0530

    Update Mainpage.js

--- components/Mainpage.js
+++ components/Mainpage.js
@@ -1,36 +1,28 @@
-'use client'
-import React from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
+'use client';
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import './mainpage.css'
 
 const Mainpage = () => {
   return (
-    <div style={{ display: "flex" }}>
-      <div style={{ marginLeft: "20rem" }}> 
-        <Image
-          src='/images/home.png'
-          alt=".."
-          width={400}
-          height={360}
-        />
+    <div className="main-container">
+      <div className="image-container">
+        <Image src='/images/home.png' alt=".." width={400} height={360} />
       </div>
-      <div style={{ textAlign: "left",  fontFamily: "Arial", marginTop: "3rem" }}>
-        
-        <span style={{ display: "inline-block", textAlign: "center", fontWeight: "bold" ,fontSize: "2rem", }}>For every student,</span><br/>
-        <span style={{ display: "inline-block", textAlign: "center", fontWeight: "bold" , fontSize: "2rem", }}>every classroom,</span><br/>
-        <span style={{ display: "inline-block", textAlign: "center", fontWeight: "bold" , fontSize: "2rem", }}>real results.</span><br/>
-        
-        
-        <span style={{ display: "inline-block", textAlign: "center" , fontSize: "1.3rem" }}>We’re a nonprofit with the mission to provide </span><br/>
-        <span style={{ display: "inline-block", textAlign: "center" , fontSize: "1.3rem" }}>a free, world-class education for anyone, </span><br/>
-        <span style={{ display: "inline-block", textAlign: "center" , fontSize: "1.3rem" }}>anywhere.</span><br/><br/>
+      <div className="text-container">
+        <span className="title">For every student,</span><br />
+        <span className="title">every classroom,</span><br />
+        <span className="title">real results.</span><br />
 
-        <button style={{ backgroundColor: "#1165ed", color: "white", padding: "10px 20px", fontSize: "1.2rem", borderRadius: "3px" }}>Start here</button>
+        <span className="subtitle">We’re a nonprofit with the mission to provide </span><br />
+        <span className="subtitle">a free, world-class education for anyone, </span><br />
+        <span className="subtitle">anywhere.</span><br /><br />
 
+        <button className="start-button">Start here</button>
       </div>
-      
-</div>
-  )
-}
+    </div>
+  );
+};
 
-export default Mainpage
+export default Mainpage;
